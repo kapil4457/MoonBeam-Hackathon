28f60099ebcb0904dc8ba55111840dad8e6a1023
Kapil Soni <kapilsoni54768161@gmail.com>
2022-09-07 12:53:21 +0530

    Created Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import {Routes , Route} from 'react-router-dom';
 import Home from './components/Home';
 import CreateNFT from './components/CreateNFT';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Route path="/" element={<Home />} />
     <Route path="/create" element={<CreateNFT />} />
     </Routes>
+    <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+const Footer = () => {
+  return (
+    <Container>
+      <Contact>
+        <h3>Contact us</h3>
+
+        <p>
+          <b>Email</b> : demo@gmail.com
+        </p>
+      </Contact>
+      <Socials>
+        <h3>Socials</h3>
+        <div>
+          <a href="https://www.instagram.com" target="blank">
+            <img src="instagram.png" alt="" />
+          </a>
+          <a href="https://www.discord.com" target="blank">
+            <img src="discord.png" alt="" />
+          </a>
+        </div>
+      </Socials>
+    </Container>
+  );
+};
+
+export default Footer;
+const Container = styled.div`
+  margin-top: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+  height: 10rem;
+  box-shadow: -1px -1px 8px 1px gray;
+`;
+
+const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  h3 {
+    font-size: 1.7rem;
+    text-decoration: underline;
+    font-weight: 500;
+    text-shadow: 1px 1px 1px gray;
+  }
+  p {
+  }
+`;
+const Socials = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  div {
+    display: flex;
+    gap: 1rem;
+  }
+
+  h3 {
+    font-size: 1.7rem;
+    text-decoration: underline;
+    font-weight: 500;
+    text-shadow: 1px 1px 1px gray;
+  }
+  img {
+    width: 2rem;
+    height: 2rem;
+  }
+`;
